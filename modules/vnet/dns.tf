913cfe85866047a15fe9789d1e8266b034a42ac8
Your Name <you@example.com>
2021-09-05 15:51:46 +0300

    4

--- modules/vnet/dns.tf
+++ modules/vnet/dns.tf
@@ -10,6 +10,12 @@ resource "azurerm_dns_a_record" "dns_a_record" {
   zone_name           = azurerm_private_dns_zone.dns_zone.name
   resource_group_name = azurerm_resource_group.rg.name
   ttl                 = 300
-  records             = azurerm_subnet.subnet[each.key].id
+  records             = azurerm_public_ip.public_ip.id
 }
 
+resource "azurerm_private_dns_zone_virtual_network_link" "privateDNS" {
+  name                  = "privateDNS"
+  resource_group_name   = azurerm_resource_group.example.name
+  private_dns_zone_name = azurerm_private_dns_zone.example.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+}
