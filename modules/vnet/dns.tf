ecae9ba680bc28b1640e5f8540d4e648af2f01c7
Your Name <you@example.com>
2021-09-05 15:40:06 +0300

    3

--- modules/vnet/dns.tf
+++ modules/vnet/dns.tf
@@ -7,7 +7,7 @@ resource "azurerm_private_dns_zone" "dns_zone" {
 resource "azurerm_dns_a_record" "dns_a_record" {
   for_each = var.config
   name                = "dns"
-  zone_name           = azurerm_dns_zone.dns_zone.name
+  zone_name           = azurerm_private_dns_zone.dns_zone.name
   resource_group_name = azurerm_resource_group.rg.name
   ttl                 = 300
   records             = azurerm_subnet.subnet[each.key].id
